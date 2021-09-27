e70cf321ac5208993c9d4dc9dd2f908733bbbd0b
Thodoris Theodorou <thotheod@microsoft.com>
2021-09-28 00:59:44 +0300

    generate crash

--- TipCalc/TipCalc/TipCalc/TipCalcPage.xaml.cs
+++ TipCalc/TipCalc/TipCalc/TipCalcPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AppCenter.Crashes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,11 @@ namespace TipCalc
         public TipCalcPage()
         {
             InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Crashes.GenerateTestCrash();
         }
     }
 }
