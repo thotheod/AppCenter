db123aa0e4eacd3f5742d121e6be504d80f9bda3
Thodoris Theodorou <thotheod@microsoft.com>
2021-09-28 00:28:41 +0300

    exception handling

--- TipCalc/TipCalc/TipCalc/TipCalcModel.cs
+++ TipCalc/TipCalc/TipCalc/TipCalcModel.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 
 namespace TipCalc
@@ -14,12 +16,12 @@ namespace TipCalc
         {
             set
             {
-                 if (subTotal != value)
-                 {
-                     subTotal = value;
-                     OnPropertyChanged("SubTotal");
-                     Recalculate();
-                 }
+                if (subTotal != value)
+                {
+                    subTotal = value;
+                    OnPropertyChanged("SubTotal");
+                    Recalculate();
+                }
             }
             get
             {
@@ -95,11 +97,29 @@ namespace TipCalc
 
         void Recalculate()
         {
-            this.TipAmount = Math.Round(this.TipPercent * this.SubTotal / 100, 2);
+            try
+            {
+                this.TipAmount = Math.Round(this.TipPercent * this.SubTotal / 100, 2);
 
-            // Round total to nearest quarter.
-            this.Total = Math.Round(4 * (this.PostTaxTotal + this.TipAmount)) / 4;
-            Analytics.TrackEvent("Recalculate called");
+                // Round total to nearest quarter.
+                this.Total = Math.Round(4 * (this.PostTaxTotal + this.TipAmount)) / 4;
+                Analytics.TrackEvent("Recalculate called");
+
+                if (this.Total > 1000)
+                {
+                    throw new Exception("Epic Money, Epic Exception =:-/");
+                }
+            }
+            catch (Exception exception)
+            {
+                var properties = new Dictionary<string, string> {
+                    { "Key", "Value pairs" },
+                    { "Wifi", "On" },
+                    { "Location Services", "On" }
+                };
+                Crashes.TrackError(exception, properties);
+            }
+
         }
 
         protected void OnPropertyChanged(string propertyName)
